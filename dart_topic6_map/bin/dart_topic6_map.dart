9d209f52cebd48a1d363c37a8c0154ab2b419d5e
AsylzhanOlpanbay <asylzhan.olpanbay@mail.ru>
2023-11-08 16:16:59 +0600

    first commit

---
+++ dart_topic6_map/bin/dart_topic6_map.dart
@@ -0,0 +1,71 @@
+
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+
+void main(List<String> arguments) {
+  // Topic 6
+  // Task 1
+  // List<int> numbers = [1, 2, 3];
+  // List<int> triplednumbers = numbers.map((int number) => number * 3).toList();
+  // print(triplednumbers);
+
+  // Task 2
+  // final showData = (List<int> n1, List<int> n2, List<int> n3) => n1 + n2 + n3;
+  // main() {
+  //   print (showData([1, 2], [3, 4], [5, 6]));
+  // }
+
+  // Task 3
+  // List<String> words = ["Arsen", "Amirzhan", "Dias"];
+  // List<List<String>> newList = words.map((String str) {
+  //   return str.split('').toList();
+  // }).toList();
+  // print(newList);
+
+  // Task 5
+  // String words = "Arsen, Daniyar";
+  // List<String> list = words.split('').map((word) {
+  //   word = word.toLowerCase();
+  //   return word;
+  // }).toSet().toList();
+  // String result = list.join();
+  // print(result);
+
+  // Task 6
+  // List<int> l1 = [1, 2, 3];
+  // List<int> l2 = [2, 3, 4];
+  // l2.map((e) => l1.remove(e)).toList();
+  // print(l1);
+
+  // Task 7
+  // List <String> listString = <String>["1", "2"];
+  // List <int> listInt = listString.map(int.parse).toList();
+  // print(listInt);
+
+  // Task 8
+  // List<int> num = [0, 1, 1, 2];
+  // List<int> number = num.map((int n) => n * n).toList();
+  // print(number);
+
+  // Task 9
+  // List<int> numbers = [1, 2, 3, 4, 5];
+  // int sum = numbers.reduce((value, element) => value + element);
+  // print(sum);
+
+  // Task 10
+  // List<int> num = [1, 2, 0, 5];
+  // int count = 0;
+  // int count2 = 0;
+  // int count3 = 0;
+  // num.forEach((i) => {if (i % 2 == 0 && i != 0) {
+  //   count++
+  // } else if (i % 2 != 0) {
+  //   count2++
+  // } else if (i == 0) {
+  //   count3++
+  // }});
+  // print(count);
+  // print(count2);
+  // print(count3);
+}
---
+++ dart_topic6_map/test/dart_topic6_map_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_topic6_map/dart_topic6_map.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
